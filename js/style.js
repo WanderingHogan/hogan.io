35137c50c37f2a8766fb3635aa5fb8effe291d7d
chris hogan <chris@hogan.io>
2018-08-09 22:28:33 -0600

    added hash and footer

--- js/style.js
+++ js/style.js
@@ -6,14 +6,35 @@ let setStyle = () => {
     // only do this if tilesize is over 600px to honor media query
     if (tileSize > 650) {
         theClass[0].style.width = `${tileSize}px`
-    }
-    else {
+    } else {
         theClass[0].style.height = `auto`
         theClass[0].style.width = `100%`
     }
 
-    // set footer location
-    // let height = document.getElementsByClassName('masonry-list')
-    // console.log(height)
-
 };
+
+let getCurrentIPFSHash = (domain) => {
+	let currentIPFSHash;
+    domain = domain || window.location.hostname;
+    if (domain !== 'localhost') {
+    	
+        fetch(`https://dns-api.org/TXT/${domain}`) // Call the fetch function passing the url of the API as a parameter
+            .then(function(d) {
+                return d.json()
+            })
+            .then(function(e) {
+                if (e.length > 0) {
+                    e.forEach((data) => {
+                        if (data.value.includes('dnslink=/ipfs/')) {
+                            currentIPFSHash = data.value.split('/ipfs/')[1];
+                            // return currentIPFSHash;
+                            let changeClass = document.getElementById("currentHash");
+                            changeClass.innerHTML = currentIPFSHash;
+                        }
+                    })
+
+                }
+            });
+    }
+    
+}
